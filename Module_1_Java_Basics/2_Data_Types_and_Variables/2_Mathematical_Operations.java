bc56ab04b93edd22b8c4407acc404e32dece3ae3
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-25 10:26:19 -0700

    Create 2_Mathematical_Operations.java

---
+++ Module_1_Java_Basics/2_Data_Types_and_Variables/2_Mathematical_Operations.java
@@ -0,0 +1,52 @@
+// Write a program to find SUM, DIFFERENCE, and MULTIPLICATION PRODUCT of two numbers (5 and 2).
+
+public class MathProblem
+{
+	public static void main(String args[])
+	{
+		int num1 = 5;
+		int num2 = 2;
+
+		int sum = 0;
+		int difference = 0;
+		int product = 0;
+
+        //write the logic to add two integers, 
+        sum = num1 + num2;
+        System.out.print("Sum: " + sum);
+        
+        //write the logic to subtract two integers to get difference
+        difference = num1 - num2;
+        System.out.print(", Difference: " + difference);
+
+        // write the logic to multiply two integers to get the product.
+        product = num1 * num2;
+        System.out.print(", Multiplication: " + product);
+
+
+        // print the sum
+
+        // print the difference
+
+        // print the product
+		
+    }
+}
+
+public class Variables {
+	public static void main(String[] args) {
+		
+		//Declare String variable and initialize a value in the variable.
+		String s ="Hello";
+		
+		//Declare int variable and initialize a value in the variable.
+		int i = 2;
+		
+		//Declare double variable and initialize a value in the variable
+        double d = 3.6;
+
+		//Print three variables using printlin statement
+		System.out.println(i ,d, s)
+
+	}
+}

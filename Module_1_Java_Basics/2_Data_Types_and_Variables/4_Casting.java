14ffe23025ebb52aa276779a0dd1fb84c35c628b
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-25 10:30:27 -0700

    Create 4_Casting.java

---
+++ Module_1_Java_Basics/2_Data_Types_and_Variables/4_Casting.java
@@ -0,0 +1,13 @@
+//Write a program to cast int value to double and double value to int.
+
+import java.util.*;
+public class Casting
+{
+	public static void main(String args[])
+    {
+        int num1 = 10; // get first Integer
+        double num2 = 16.72; // get second double
+        //write the logic to caste int to double and doube to int
+        System.out.println((double)num1 +","+(int)num2);
+    }
+}

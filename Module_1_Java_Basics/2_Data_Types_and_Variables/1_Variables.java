eb0889077df7931cb3717589439df1564773a3b7
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-25 10:23:26 -0700

    Create 1_Variables.java

---
+++ Module_1_Java_Basics/2_Data_Types_and_Variables/1_Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+	public static void main(String[] args) {
+		
+		//Declare String variable and initialize a value in the variable.
+		String s ="Hello";
+		
+		//Declare int variable and initialize a value in the variable.
+		int i = 2;
+		
+		//Declare double variable and initialize a value in the variable
+        double d = 3.6;
+
+		//Print three variables using printlin statement
+		System.out.println("Value: " + i + ", "+ d + " and " + s);
+
+	}
+}

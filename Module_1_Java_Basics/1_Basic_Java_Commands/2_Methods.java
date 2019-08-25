3896e50bdefdab97584882544b504566c1586098
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-25 10:15:21 -0700

    Create 2_Methods.java

---
+++ Module_1_Java_Basics/1_Basic_Java_Commands/2_Methods.java
@@ -0,0 +1,14 @@
+//Write a method named as toPrint(), this method needs to be called from main method
+//and print the ‘Print Method’ message using println statement.
+
+public class MethodCall {
+    public static void main(String[] args) {
+        //Call the toPrint() method here
+        toPrint();
+    }
+    public static void toPrint(){
+        System.out.println("Print Method");
+    }
+	
+    //Create a toPrint() here and in the method body write a print statement to print 'Print Method'
+}

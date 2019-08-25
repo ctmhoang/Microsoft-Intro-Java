08349d0e3c4c558dcea7bae5924bf5375674c189
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-25 10:13:32 -0700

    Create 1_Strings_and_Printlns.java

---
+++ Module_1_Java_Basics/1_Basic_Java_Commands/1_Strings_and_Printlns.java
@@ -0,0 +1,7 @@
+//Write a program to print ‘Helloworld’
+public class Helloworld {
+	public static void main(String args[]) {
+	    //Print “Helloworld” using a print statement
+	    System.out.println("Helloworld");
+    }
+}

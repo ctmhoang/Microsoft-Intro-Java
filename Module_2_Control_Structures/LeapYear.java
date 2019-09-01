5f894cfe6f6f2deb6fbc60b319292d93f67c964f
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-08-31 23:32:49 -0700

    Create LeapYear.java

---
+++ Module_2_Control_Structures/LeapYear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class LeapYear
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int num = s.nextInt();
+        //boolean flag = false;
+        // Write logic to find given number is divisible by 5 or not
+        if (num % 5 == 0){
+            System.out.println("TRUE");
+        }
+        else{
+            System.out.println("FALSE");
+        }
+
+        // Print flag value
+    }
+}

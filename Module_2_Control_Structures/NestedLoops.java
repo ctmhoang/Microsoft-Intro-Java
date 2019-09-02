72c0e9ec0c666ea1a33a9f18ab3449aef4130da7
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-09-01 21:11:20 -0700

    Create NestedLoops.java

---
+++ Module_2_Control_Structures/NestedLoops.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        // Write the logic using nested loop.
+        String[] words = str.split("\\s+");
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                if(Character.isUpperCase(word.charAt(i))){
+                    System.out.print(Character.toLowerCase(word.charAt(i)));
+                }
+                else{
+                    System.out.print(Character.toUpperCase(word.charAt(i)));
+                }
+            }
+            System.out.print(" ");
+        }
+    }
+}
+
+//Remerber /s /S /s+ /s*

84a4c37ce294146c5a0ad5327398231eeacc62fd
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-09-14 08:45:28 -0700

    Update Recursion.java

--- Module_3_Data_Flow/Recursion.java
+++ Module_3_Data_Flow/Recursion.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Recursion
 {
-	   public static void main(String args[]) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         int sum = 0;
